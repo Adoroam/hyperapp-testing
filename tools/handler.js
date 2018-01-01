2f1fbd6e864845fc386aaf65520b236398f3fbab
Anthony Stabile <astabile.design@gmail.com>
2017-12-31 20:48:37 -0600

    moved 2 routing functions to handler.

--- tools/handler.js
+++ tools/handler.js
@@ -1,8 +1,11 @@
+const path = require('path')
 module.exports = {
   mwStatus: (req, res, next) => {
     next()
   },
   toApp: (req, res) => {
     res.sendFile('index.html')
-  }
+  },
+  mockJson: (req, res) => { res.json({test: 'success'}) },
+  nodePassthrough: (req, res) => { res.sendFile(path.join(__dirname, '..', req.path)) }
 }
