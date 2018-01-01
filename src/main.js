7af6d15cf887d7b284d1bc84ec320facfd375ed0
Anthony Stabile <ado@Dent.homeworld.pw>
2018-01-01 01:03:59 -0600

    moved MobilePlate to new components dir.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { h, app } from 'hyperapp'
+import { MobilePlate } from './components/MobilePlate.js'
 
 const state = {
   plates: [
@@ -92,33 +93,8 @@ const actions = {
     if ((t.ex < t.sx) && (t.sx - t.ex > 10)) a.decUpdate(eObj.plate) // left
   }
 }
-// PLATE
-const MobilePlate = ({name, price, qty, hexCode, inc, ts, te}) => (
-  <div
-    class='pmd-card pmd-card-default pmd-z-depth'
-    style={{backgroundColor: hexCode}}
-    ontouchstart={ts}
-    ontouchend={te}
-    >
-    <div class='pmd-card-title'>
-      <div class='media-left'>
-        <a class='qty'>{qty}</a>
-      </div>
-      <div class='media-body media-middle'>
-        <h3 class='pmd-card-title-text'>${(price * qty).toFixed(2)}</h3>
-        <span class='pmd-card-subtitle-text'>${price}</span>
-      </div>
-      <div class='media-right'>
-        <button
-          class='btn btn-sm pmd-btn-fab pmd-ripple-effect btn-default inc-button'
-          type='button'
-          onclick={inc}>
-          <i class='material-icons pmd-sm'>add</i>
-        </button>
-      </div>
-    </div>
-  </div>
-)
+// PLATES
+
 const view = (s, a) => (
   <div class='the-parent'>
     {/* MOBILE */}
